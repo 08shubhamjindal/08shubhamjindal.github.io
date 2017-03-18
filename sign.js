573f6c3127c1dbc3daa8425161d3ff8f82f23615
08shubhamjindal <shubhamjindal.cse19@jecrc.ac.in>
2017-03-18 23:41:26 +0530

    facebook

---
+++ sign.js
@@ -0,0 +1,26 @@
+var provider = new firebase.auth.FacebookAuthProvider();
+
+function facebookSignin() {
+   firebase.auth().signInWithPopup(provider)
+   
+   .then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+		
+      console.log(token)
+      console.log(user)
+   }).catch(function(error) {
+      console.log(error.code);
+      console.log(error.message);
+   });
+}
+
+function facebookSignout() {
+   firebase.auth().signOut()
+   
+   .then(function() {
+      console.log('Signout successful!')
+   }, function(error) {
+      console.log('Signout failed')
+   });
+}
